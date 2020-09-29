bc49df50398a7416b2f2cc0b472d8dbc07ee51ca
Adrien <adrien@localhost.localdomain>
2020-09-29 11:08:14 +0200

    .

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+// import dependencies
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express() // create your express app
+
+// make app use dependencies
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.listen(process.env.PORT || 8081) // client is already running on 8080
+
