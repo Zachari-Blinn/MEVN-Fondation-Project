fb30df5f7cd208e0447a6a5e4b4e45559fcb43f4
Blinn Zachari <zacblinn@hotmail.fr>
2020-10-04 20:34:59 +0200

    .

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Navigation />
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Navigation from './components/partials/Navigation'
+import Footer from './components/partials/Footer'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navigation,
+    Footer
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue !'
+  }
+})
+</script>
