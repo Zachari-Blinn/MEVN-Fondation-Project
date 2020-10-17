65564ea8d715f9a0e83fbbf928d6b32add87532e
root <zacblinn@hotmail.frca>
2020-10-18 00:12:11 +0200

    add people edit of back office

--- client/src/components/partials/Admin_Navigation.vue
+++ client/src/components/partials/Admin_Navigation.vue
@@ -1,13 +1,36 @@
 <template>
-  
+  <div>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/admin/people">People</router-link></li>
+          <li><router-link to="/admin/company">Company</router-link></li>
+          <li>
+            <router-link to="/admin/advertisement">advertisement</router-link>
+          </li>
+          <li><router-link to="/admin/candidacy">Candidacy</router-link></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  mounted() {
+    M.AutoInit();
+  },
+};
 </script>
 
 <style>
-
+footer {
+  position: absolute;
+  bottom: 0 !important;
+}
+nav {
+  margin-bottom: 75px;
+}
 </style>
--- client/src/components/partials/Navigation.vue
+++ client/src/components/partials/Navigation.vue
@@ -1,19 +1,61 @@
 <template>
   <div>
-    <h2>Nav Bar</h2>
     <nav>
-      <router-link to="/home">Home</router-link>
-      <router-link to="/company/create">Company create</router-link>
-      <router-link to="/company/index">Company index</router-link>
-      <router-link to="/advertisement/index">Advertisement index</router-link>
-      <span v-if="isLoggedIn"> |
-        <a :href="people_url_show + user._id">User page</a> | 
-        <a @click="logout">Logout</a>
-      </span>
-      <span v-else> | <router-link to="/login">Login</router-link></span>
-      <span v-if="!isLoggedIn"> |
-      <router-link to="/register">Register</router-link></span>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"
+          ><i class="material-icons">menu</i></a
+        >
+        <ul class="right hide-on-med-and-down">
+          <li><router-link to="/">Home</router-link></li>
+          <li>
+            <router-link to="/company/create">Company create</router-link>
+          </li>
+          <li><router-link to="/company/index">Company index</router-link></li>
+          <li>
+            <router-link to="/advertisement/index"
+              >Advertisement index</router-link
+            >
+          </li>
+          <li>
+            <span v-if="isLoggedIn"
+              ><a :href="people_url_show + user._id">User page</a
+              ><a @click="logout">Logout</a></span
+            >
+          </li>
+          <li>
+            <span v-if="!isLoggedIn"
+              ><router-link to="/login">Login</router-link></span
+            >
+          </li>
+          <li>
+            <span v-if="!isLoggedIn"
+              ><router-link to="/register">Register</router-link></span
+            >
+          </li>
+        </ul>
+      </div>
     </nav>
+
+    <ul class="sidenav" id="mobile-demo">
+      <li><router-link to="/">Home</router-link></li>
+      <li>
+        <router-link to="/company/create">Company create</router-link>
+      </li>
+      <li><router-link to="/company/index">Company index</router-link></li>
+      <li>
+        <router-link to="/advertisement/index">Advertisement index</router-link>
+      </li>
+      <li v-if="isLoggedIn">
+        <a :href="people_url_show + user._id">User page</a><a @click="logout">Logout</a>
+      </li>
+      <li v-if="!isLoggedIn">
+        <router-link to="/login">Login</router-link>
+      </li>
+      <li v-if="!isLoggedIn">
+        <router-link to="/register">Register</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,10 +64,10 @@ import store from "../../store";
 
 export default {
   store,
-    data() {
+  data() {
     return {
       people_url_show: "http://localhost:8080/user/show/",
-      user: JSON.parse(localStorage.getItem('people'))
+      user: JSON.parse(localStorage.getItem("people")),
     };
   },
   computed: {
@@ -40,5 +82,14 @@ export default {
       });
     },
   },
+  mounted() {
+    M.AutoInit();
+  },
 };
 </script>
+
+<style>
+nav {
+  margin-bottom: 75px;
+}
+</style>
